273350fcb90ca70be9195d1a3e0eeefbc5aec393
SP4R0W <sp4r0wxxx@gmail.com>
2025-02-03 22:32:23 +0100

    Added game select panel

--- src/MainMenu/main_menu.gd
+++ src/MainMenu/main_menu.gd
@@ -1,7 +1,9 @@
 extends Control
 
+@onready var level_select_panel: TextureRect = $LevelSelectPanel
+
 func _on_play_button_pressed() -> void:
-	pass # Replace with function body.
+	level_select_panel.show()
 
 func _on_options_button_pressed() -> void:
 	Composer.load_scene("res://src/Settings/Settings.tscn")
@@ -11,3 +13,15 @@ func _on_help_button_pressed() -> void:
 
 func _on_quit_button_pressed() -> void:
 	get_tree().quit()
+
+
+func _on_x_button_pressed() -> void:
+	level_select_panel.hide()
+
+
+func _on_new_game_button_pressed() -> void:
+	pass # Replace with function body.
+
+
+func _on_level_select_button_pressed() -> void:
+	pass # Replace with function body.
