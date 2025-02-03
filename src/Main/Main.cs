475447b50cf005e1f4f3b5ace3d77bfcbd92fc5e
VargaDot <vancouver_cliparf@simplelogin.com>
2025-02-03 21:26:28 +0100

    converted main to gdscript

--- src/Main/Main.cs
+++
@@ -1,9 +0,0 @@
-using Godot;
-
-public partial class Main : Control
-{
-    public override void _Ready()
-    {
-        Global.Composer.Call("load_scene", "res://src/MainMenu/MainMenu.tscn");
-    }
-}
