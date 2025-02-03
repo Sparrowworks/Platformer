e933708c2031e25b3d03f98e747a8f5791fa5f52
SP4R0W <sp4r0wxxx@gmail.com>
2025-02-03 14:15:33 +0100

    Booting into mainmenu

--- src/Main/Main.cs
+++ src/Main/Main.cs
@@ -5,6 +5,8 @@ public partial class Main : Control
 {
     public override void _Ready()
     {
-
+        Composer Composer = GetNode<Composer>("/root/Composer");
+        Composer.Root = this;
+        Composer.LoadScene("res://src/MainMenu/MainMenu.tscn");
     }
 }
