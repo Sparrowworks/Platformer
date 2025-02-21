5bca843d125e6f719792200a25b3de28d5b919e6
SP4R0W <sp4r0wxxx@gmail.com>
2025-02-21 14:11:45 +0100

    Fix for gameend

--- src/GameEnd/game_end.gd
+++ src/GameEnd/game_end.gd
@@ -9,7 +9,8 @@ extends Control
 
 func _ready() -> void:
 	if Globals.is_new_game:
-		desc.text = ""
+		desc.text = "Congratulations!
+		You have managed to find the lost ufo ship and now you can return to your home planet!"
 		title.text = "Game Stats:"
 		if Globals.is_speedrun_mode:
 			time_text.text = "Total Time: " + str(Globals.level_speedrun_times[6])
