da18fec8689ca49e3b3e701638b21cb1a04cc6e0
SP4R0W <sp4r0wxxx@gmail.com>
2025-05-31 12:54:47 +0200

    Update src/Game/Enemies/Bat/bat.gd
    Co-authored-by: Varga <110741996+VargaDot@users.noreply.github.com>

--- src/Game/Enemies/Bat/bat.gd
+++ src/Game/Enemies/Bat/bat.gd
@@ -67,7 +67,7 @@ func _on_player_tracked() -> void:
 	if not is_chasing:
 		is_chasing = true
 
-		# Calculate the position of where to fly and the flight time of the bat
+		# Calculate the bat's target position and flight time.
 		var desired_pos: Vector2 = Vector2(Globals.player.global_position.x, Globals.player.global_position.y)
 		var fly_time: float = clampf((desired_pos.y - initial_pos.y)/2000, 1, 1.5)
 		animated_sprite_2d.play("fly")
